fb0fd1ae8093da0dffed280ded51fe06d6282ac7
JoaquinLiHua <JoaquinLiHua@protonmail.com>
2020-09-12 00:58:43 +1000

    update news

--- src/components/general/NewsBlock.tsx
+++ src/components/general/NewsBlock.tsx
@@ -4,10 +4,7 @@ import { Text, Stack } from "@chakra-ui/core";
 export const NewsBlock = () => (
   <Stack my="4" spacing={2}>
     <Text fontSize="sm" textAlign="center">
-      LATEST: FIRST GOVERNANCE PROPOSAL (BFIP-02) LIVE
-    </Text>
-    <Text fontSize="sm" textAlign="center">
-      BOOST-ETH POOL LIVE (3.75X REWARDS!
+      LATEST: BOOST-ETH POOL LIVE 3.75X REWARDS!
     </Text>
     <Text fontSize="sm" textAlign="center">
       GENESIS POOLS HAVE ENDED, WITHDRAW YOUR STAKED ASSETS!
