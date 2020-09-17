524b47f418778ae3f234efe18c5af913311fa88c
JoaquinLiHua <JoaquinLiHua@protonmail.com>
2020-09-17 22:52:34 +1000

    add disclaimer into news block

--- src/components/general/NewsBlock.tsx
+++ src/components/general/NewsBlock.tsx
@@ -4,10 +4,15 @@ import { Text, Stack } from "@chakra-ui/core";
 export const NewsBlock = () => (
   <Stack my="4" spacing={2}>
     <Text fontSize="sm" textAlign="center">
-      LATEST: BOOST-ETH POOL LIVE 3.75X REWARDS!
+      There have been no official third-party audits for Boosted Finance
+      although core contributors have made extensive efforts to secure smart
+      contracts including forking the codebases of notable and established
+      projects.
     </Text>
     <Text fontSize="sm" textAlign="center">
-      GENESIS POOLS HAVE ENDED, WITHDRAW YOUR STAKED ASSETS!
+      ⚠️ We urge all users who engage with staking contracts to self-audit and
+      read through contracts before putting your LP tokens at stake. You will be
+      using this BETA product at your own risk.
     </Text>
   </Stack>
 );
