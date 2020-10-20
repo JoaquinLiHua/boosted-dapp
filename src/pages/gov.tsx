2bbfa63ec453bdecde5f1f8a2b8f216b3ef26e1f
JoaquinLiHua <JoaquinLiHua@protonmail.com>
2020-10-20 22:54:55 +1100

    add governance coming soon

--- src/pages/gov.tsx
+++ src/pages/gov.tsx
@@ -1,7 +1,13 @@
+import { Stack, Heading, Text } from "@chakra-ui/core";
 import React from "react";
 
 const Gov: React.FC = () => {
-  return <></>;
+  return (
+    <Stack spacing={4} mt={4} width="100%">
+      <Heading>Governance</Heading>
+      <Text>Our migrated governance contracts are coming soon... 🚀</Text>
+    </Stack>
+  );
 };
 
 export default Gov;
