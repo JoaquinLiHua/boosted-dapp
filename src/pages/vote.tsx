6b1c62835a16a3133dd25133ec429803a7be71f7
JoaquinLiHua <JoaquinLiHua@protonmail.com>
2020-11-01 22:47:40 +1100

    bring back old gov for BFIPs

--- src/pages/vote.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Vote as VoteComponent } from "src/components/vote/Vote";
-
-const Vote: React.FC = () => {
-  return <VoteComponent />;
-};
-
-export default Vote;
