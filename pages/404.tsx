060e14426345f70b9f7de7bb742652ccc6b24533
fibbpepe <fibbpepe@gmail.com>
2021-02-11 01:39:01 +0200

    404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,51 @@
-import { Heading, Image, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
+import styled from 'styled-components';
+
+import { H1, PrimaryButton } from 'styles/common';
 
 const NotFound: React.FC = () => {
 	return (
-		<Stack spacing={6} alignItems="center">
-			<Image src="/images/404-astronaut.jpg" w={100} h={100} borderRadius="full" />
-			<Heading textAlign="center">404 - Not Found</Heading>
-			<Text fontWeight="bold">It looks like you&apos;re lost in space.</Text>
-		</Stack>
+		<>
+		<Container>
+			<Telescope src="/images/Telescope.png" />
+			<Title404>404 - Oh no, destination not found</Title404>
+			<P>Well, this is awkward... The link you followed may be broken, or the page may have been removed.</P>
+			<Button href="/">Back to the home planet</Button>
+		</Container>
+		</>
 	);
 };
 
 export default NotFound;
+
+const Container = styled.div`
+	margin-top: 10vw;
+	text-align: center;
+`;
+
+const Title404 = styled(H1)`
+	
+	margin-bottom: 0;
+	font-family: ${(props) => props.theme.fonts.interSemiBold};
+`;
+
+const Telescope = styled.img`
+	margin: 0 auto;
+	display: flex;
+	max-width: 128px;
+	width: 80%;
+	margin-bottom: 24px;
+`;
+
+const P = styled.p`
+	margin-top: 8px;
+	margin-bottom: 48px;
+	font-size: 14px;
+	color: #B3B3B3;
+	letter-spacing: 0;
+	line-height: 24px;
+	font-family: ${(props) => props.theme.fonts.interMedium};
+`;
+
+const Button = styled(PrimaryButton)`
+`;
